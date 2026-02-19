1dfcbc17055ea00f980756c81ae6aaa427717a7d
gferreira <gustavo@hipertipo.com>
2026-02-19 19:09:12 -0300

    rebuilding glyph meme proofs for control glyphs (Roman & Italic)

--- Tools/proofing/proof-glyph-memes.py
+++ Tools/proofing/proof-glyph-memes.py
@@ -5,7 +5,7 @@ reload(xTools4.modules.glyphMemeProofer)
 import os
 from xTools4.modules.glyphMemeProofer import GlyphMemeProofer
 
-subFamilyName = ['Roman', 'Italic'][1]
+subFamilyName = ['Roman', 'Italic'][0]
 baseFolder = os.path.dirname(os.path.dirname(os.getcwd()))
 sourcesFolder = os.path.join(baseFolder, 'Sources', subFamilyName)
 proofsFolder =  os.path.join(baseFolder, 'Proofs', 'PDF', 'glyph-memes')
@@ -14,17 +14,15 @@ savePDF = False
 
 assert os.path.exists(sourcesFolder)
 
-designspacePath = os.path.join(sourcesFolder, f'AmstelvarA2-{subFamilyName}_avar2.designspace')
+designspacePath = os.path.join(sourcesFolder, f'AmstelvarA2-{subFamilyName}.designspace')
 
-for glyphName in 'T':
+for glyphName in 'H':
 
     P = GlyphMemeProofer(glyphName, designspacePath)
-    P.anchorsDraw = False
+    P.anchorsDraw = True
     P.draw()
 
-    pdfFileName = f'glyph-memes_{subFamilyName}'
+    pdfFileName = os.path.splitext(os.path.split(designspacePath)[-1])[0]
 
     if savePDF:
         P.save(proofsFolder, pdfFileName)
-
-    # newDrawing()
