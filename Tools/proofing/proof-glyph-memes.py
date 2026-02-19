5cceae9ba2f777dea4df12ce34226c177fedc5a5
gferreira <gustavo@hipertipo.com>
2026-02-19 19:21:12 -0300

    [Italic] rebuilding glyph meme proofs with correct italic angle deltas

--- Tools/proofing/proof-glyph-memes.py
+++ Tools/proofing/proof-glyph-memes.py
@@ -5,7 +5,7 @@ reload(xTools4.modules.glyphMemeProofer)
 import os
 from xTools4.modules.glyphMemeProofer import GlyphMemeProofer
 
-subFamilyName = ['Roman', 'Italic'][0]
+subFamilyName = ['Roman', 'Italic'][1]
 baseFolder = os.path.dirname(os.path.dirname(os.getcwd()))
 sourcesFolder = os.path.join(baseFolder, 'Sources', subFamilyName)
 proofsFolder =  os.path.join(baseFolder, 'Proofs', 'PDF', 'glyph-memes')
@@ -16,7 +16,7 @@ assert os.path.exists(sourcesFolder)
 
 designspacePath = os.path.join(sourcesFolder, f'AmstelvarA2-{subFamilyName}.designspace')
 
-for glyphName in 'H':
+for glyphName in 'HOVTnov':
 
     P = GlyphMemeProofer(glyphName, designspacePath)
     P.anchorsDraw = True
