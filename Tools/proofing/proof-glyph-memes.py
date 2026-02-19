8afeb02ee7f12019bae8d31fc6b39106476efa01
gferreira <gustavo@hipertipo.com>
2026-02-19 16:17:37 -0300

    renaming designspace files

--- Tools/proofing/proof-glyph-memes.py
+++ Tools/proofing/proof-glyph-memes.py
@@ -5,20 +5,21 @@ reload(xTools4.modules.glyphMemeProofer)
 import os
 from xTools4.modules.glyphMemeProofer import GlyphMemeProofer
 
-subFamilyName = ['Roman', 'Italic'][0]
+subFamilyName = ['Roman', 'Italic'][1]
 baseFolder = os.path.dirname(os.path.dirname(os.getcwd()))
 sourcesFolder = os.path.join(baseFolder, 'Sources', subFamilyName)
 proofsFolder =  os.path.join(baseFolder, 'Proofs', 'PDF', 'glyph-memes')
 
-savePDF = True
+savePDF = False
 
 assert os.path.exists(sourcesFolder)
 
 designspacePath = os.path.join(sourcesFolder, f'AmstelvarA2-{subFamilyName}_avar2.designspace')
 
-for glyphName in 'nov':
+for glyphName in 'T':
 
     P = GlyphMemeProofer(glyphName, designspacePath)
+    P.anchorsDraw = False
     P.draw()
 
     pdfFileName = f'glyph-memes_{subFamilyName}'
@@ -26,4 +27,4 @@ for glyphName in 'nov':
     if savePDF:
         P.save(proofsFolder, pdfFileName)
 
-    newDrawing()
+    # newDrawing()
