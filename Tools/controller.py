7aaa9a90a9b1db36033cf29c81e97e983c9e8634
gferreira <gustavo@hipertipo.com>
2026-09-09 12:32:08 -0300

    [Roman] fixing /eng tuning, rebuilding variable font

--- Tools/controller.py
+++ Tools/controller.py
@@ -665,10 +665,10 @@ if __name__ == '__main__':
 
     referenceSource = os.path.join(p.referenceSourcesFolder, 'deprecated', f'Amstelvar-{subFamily}_wght400.ufo')
 
-    # glyphNames = 'NJ LJ Nj Lj DZcaron Dzcaron IJ IJacute Iacute_J.loclNLD ijacute iacute_j.loclNLD lj nj'.split()
+    glyphNames = ['eng']
     # glyphNames = 'dollar cent guarani colonsign cedi peso won kip naira'.split()
     # glyphNames = parseGString(p.defaultFont, '/ae/OE')
-    glyphNames = p.smartSets['figures']['oldstyle']
+    # glyphNames = p.smartSets['figures']['oldstyle']
     # glyphNames = p.smartSets['uppercase']['latin'] + p.smartSets['lowercase']['latin']
     # glyphNames = [g for g in glyphNames if g not in p.smartSets['Latin 1']]
     # print(glyphNames)
@@ -694,11 +694,11 @@ if __name__ == '__main__':
     # p.extractMeasurements()
 
     # --- build designspace ---
-    # p.parametricAxesHidden = True
-    # p.tuningAxesHidden = True
-    # p.tuning = True # also used to direct BlendsPreview proof to its folder
-    # p.useLongAxisNames = False # keep it disabled during development!
-    # p.buildDesignspace(instances=True, parentParametric=True)
+    p.parametricAxesHidden = True
+    p.tuningAxesHidden = True
+    p.tuning = True # also used to direct BlendsPreview proof to its folder
+    p.useLongAxisNames = True # keep it disabled during development!
+    p.buildDesignspace(instances=True, parentParametric=True)
     # p.validateDesignspace(locations=True, mappings=True, instances=False)
     # p.validateSources(parametric=False, tuning=False, reference=True)
 
@@ -710,7 +710,7 @@ if __name__ == '__main__':
 
     # --- normalization ---
     # p.cleanupSources(parametric=False, tuning=False, reference=False)
-    # p.normalizeSources(parametric=False, tuning=False, reference=True)
+    p.normalizeSources(parametric=True, tuning=True, reference=False)
 
     # --- project info ---
     # p.printSettings()
